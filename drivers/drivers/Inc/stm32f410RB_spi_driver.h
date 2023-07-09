dd4d4e1a2a617c977ee1d96866a2d4f0900b94ce
devinschubert14 <59582670+devinschubert14@users.noreply.github.com>
2023-07-08 18:51:56 -0700

    Update stm32f410RB_spi_driver.h

--- drivers/drivers/Inc/stm32f410RB_spi_driver.h
+++ drivers/drivers/Inc/stm32f410RB_spi_driver.h
@@ -3,7 +3,8 @@
 
 #include "stm32f410RB.h"
 
-typedef struct {
+typedef struct
+{
 	uint8_t SPI_DeviceMode;
 	uint8_t SPI_BusConfig;
 	uint8_t SPI_SclkSpeed;
@@ -11,61 +12,83 @@ typedef struct {
 	uint8_t SPI_CPOL;
 	uint8_t SPI_CPHA;
 	uint8_t SPI_SSM;
-}SPI_Config_t;
+} SPI_Config_t;
 
-typedef struct{
+typedef struct
+{
 	SPI_RegDef_t *pSPIx;
 	SPI_Config_t SPIConfig;
-}SPI_Handle_t;
+	uint8_t *pTxBuffer;
+	uint8_t *pRxBuffer;
+	uint8_t tx_len;
+	uint8_t rx_len;
+	uint8_t tx_state;
+	uint8_t rx_state;
+} SPI_Handle_t;
 
 /*
  * @SPI_DeviceMode
  */
 #define SPI_DEVICE_MODE_MASTER 1
-#define SPI_DEVICE_MODE_SLAVE  0
+#define SPI_DEVICE_MODE_SLAVE 0
+
+/*
+ * @SPI_AppStates
+ */
+#define SPI_READY 0
+#define SPI_BUSY_IN_RX 1
+#define SPI_BUSY_IN_TX 2
+
+/*
+ * @SPI_AppEvents
+ */
+#define SPI_EVENT_TX_CMPLT 1
+#define SPI_EVENT_RX_CMPLT 2
+#define SPI_EVENT_OVR_ERR 3
+#define SPI_EVENT_CRC_ERR 4
 
 /*
  * #SPI_BusConfig
  */
-#define SPI_BUS_CONFIG_FD				1
-#define SPI_BUS_CONFIG_HD				2
-#define SPI_BUS_CONFIG_SIMPLEX_RXONLY			3
+#define SPI_BUS_CONFIG_FD 1
+#define SPI_BUS_CONFIG_HD 2
+#define SPI_BUS_CONFIG_SIMPLEX_RXONLY 3
 
 /*
  * @SPI_SclkSpeed
  */
-#define SPI_SCLK_SPEED_DIV2				0
-#define SPI_SCLK_SPEED_DIV4				1
-#define SPI_SCLK_SPEED_DIV8				2
-#define SPI_SCLK_SPEED_DIV16				3
-#define SPI_SCLK_SPEED_DIV32				4
-#define SPI_SCLK_SPEED_DIV64				5
-#define SPI_SCLK_SPEED_DIV128				6
-#define SPI_SCLK_SPEED_DIV256				7
+#define SPI_SCLK_SPEED_DIV2 0
+#define SPI_SCLK_SPEED_DIV4 1
+#define SPI_SCLK_SPEED_DIV8 2
+#define SPI_SCLK_SPEED_DIV16 3
+#define SPI_SCLK_SPEED_DIV32 4
+#define SPI_SCLK_SPEED_DIV64 5
+#define SPI_SCLK_SPEED_DIV128 6
+#define SPI_SCLK_SPEED_DIV256 7
 
 /*
  * @SPI_DFF
  */
-#define SPI_DFF_8BITS					0
-#define SPI_DFF_16BITS					1
+#define SPI_DFF_8BITS 0
+#define SPI_DFF_16BITS 1
 
 /*
  * @CPOL
  */
-#define SPI_CPOL_HIGH 					1
-#define SPI_CPOL_LOW					0
+#define SPI_CPOL_HIGH 1
+#define SPI_CPOL_LOW 0
 
 /*
  * @CPHA
  */
-#define SPI_CPHA_HIGH 					1
-#define SPI_CPHA_LOW					0
+#define SPI_CPHA_HIGH 1
+#define SPI_CPHA_LOW 0
 
 /*
  * @SPI_SSM
  */
-#define SPI_SSM_EN					1
-#define SPI_SSM_DI					0
+#define SPI_SSM_EN 1
+#define SPI_SSM_DI 0
 
 /*
  * SPI related status flag macros
@@ -90,7 +113,6 @@ typedef struct{
  */
 void SPI_PeriClockControl(SPI_RegDef_t *pSPIx, uint8_t EnOrDi);
 
-
 /*
  * Init and De-init
  */
@@ -103,6 +125,8 @@ void SPI_DeInit(SPI_RegDef_t *pSPIx);
 void SPI_SendData(SPI_RegDef_t *pSPIx, uint8_t *pTxBuffer, uint32_t len);
 void SPI_ReceiveData(SPI_RegDef_t *pSPIx, uint8_t *pRxBuffer, uint32_t len);
 
+uint8_t SPI_SendDataIT(SPI_Handle_t *pSPIx, uint8_t *pTxBuffer, uint32_t len);
+uint8_t SPI_ReceiveDataIT(SPI_Handle_t *pSPIx, uint8_t *pRxBuffer, uint32_t len);
 /*
  * IRQ Configuration and ISR Handling
  */
@@ -117,4 +141,14 @@ uint8_t SPI_GetFlagStatus(SPI_RegDef_t *pSPIx, uint32_t flag_name);
 void SPI_PeripheralControl(SPI_RegDef_t *pSPIx, uint8_t EnOrDi);
 void SPI_SSIConfig(SPI_RegDef_t *pSPIx, uint8_t EnOrDi);
 void SPI_SSOEConfig(SPI_RegDef_t *pSPIx, uint8_t EnOrDi);
+void SPI_ClearOVRFlag(SPI_RegDef_t *PSPIx);
+void SPI_CloseTransmission(SPI_Handle_t *pSPIHandle);
+void SPI_CloseReception(SPI_Handle_t *pSPIHandle);
+
+/*
+ * Application Callback
+ */
+void SPI_ApplicationEventCallback(SPI_Handle_t *pSPIHandle, uint8_t AppEv);
+
 #endif
+
